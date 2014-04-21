06907cefc0e20a242f53b4aaecde53f5e3ceb809
maochunyang <maochunyang@cyou-inc.com>
2014-04-21 20:44:08 +0800

    Mono 引用库追加。

---
+++ Demo/Android/MpTestDemo/assets/MoTest/MoTest/FClassTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace MoTest
+{
+   public class FClassTest
+   {
+      public int _testValue = 11;
+
+      public string _testString = "hello2";
+
+      //[DllImport("__Internal", EntryPoint = "CalculateNumber")]
+      [MethodImplAttribute(MethodImplOptions.InternalCall)]
+      public static extern int CalculateNumber(object owner, int value);
+
+      public int Test(){
+         return CalculateNumber(this, 11);
+      }
+   }
+}
