acdd3e77ae290de6105e1fd20d09b2e14b084187
favedit <favedit@hotmail.com>
2014-04-26 19:33:03 +0800

    编辑器工程追加。

---
+++ Utiliy/Editor/MpSceneEditor/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using MO.SceneEditor.Face.Forms;
+using System.Windows;
+
+namespace MpSceneEditor
+{
+   /// <summary>
+   /// App.xaml 的交互逻辑
+   /// </summary>
+   public partial class App : Application
+   {
+      protected override void OnStartup(StartupEventArgs e) {
+         base.OnStartup(e);
+         // 启动窗口
+         WConsole console = new WConsole();
+         console.Show();
+      }
+   }
+}
