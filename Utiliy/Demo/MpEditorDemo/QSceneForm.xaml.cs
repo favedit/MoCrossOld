8718c96a09e5414f16dc5f9c8e839bec0b1db4f6
maochunyang <maochunyang@cyou-inc.com>
2014-04-29 19:13:06 +0800

    实例项目增加。

---
+++ Utiliy/Demo/MpEditorDemo/QSceneForm.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace MpEditorDemo
+{
+   /// <summary>
+   /// MainWindow.xaml 的交互逻辑
+   /// </summary>
+   public partial class QSceneForm : Window
+   {
+      public QSceneForm() {
+         InitializeComponent();
+      }
+   }
+}
---
+++ Utiliy/Demo/MpEditorDemo/QTemplateForm.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MpEditorDemo
+{
+   /// <summary>
+   /// MainWindow.xaml 的交互逻辑
+   /// </summary>
+   public partial class QTemplateForm : Window
+   {
+      public QTemplateForm() {
+         InitializeComponent();
+      }
+
+      private void TextBox_TextChanged(object sender, TextChangedEventArgs e) {
+
+      }
+
+      private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e) {
+
+      }
+   }
+}
