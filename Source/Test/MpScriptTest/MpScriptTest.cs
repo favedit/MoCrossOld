33766dc706f70ec2ba780e2861dc8bc003d651c3
maochunyang <maochunyang@cyou-inc.com>
2014-06-19 09:14:33 +0800

    测试代码建立。

---
+++ Source/Test/MpScriptTest/MpScriptTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MP.Script.Helper
+{
+   static class Program
+   {
+      [STAThread]
+      static void Main() {
+         Console.WriteLine("Hello");
+      }
+   }
+}
---
+++ Source/Utility/ScriptHelper/MpScriptHelper/MpScriptHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MP.Script.Helper
+{
+   static class Program
+   {
+      [STAThread]
+      static void Main() {
+      }
+   }
+}
